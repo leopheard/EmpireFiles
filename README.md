8162d0afab7be79db2a8ee22b62dca3928f78083
leopheard <leopheard.misc@gmail.com>
2019-07-14 13:04:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# plugin.video.empirefiles
+#The Empire Files
+=============================
+Kodi video addon for The Empire Files investigative journalism videos
+
+**Install**
+
+Launch Kodi >> Add-ons >> Package Icon >> Install from Zip
+Or alternatively use the search feature at the top of the main Kodi page, search for the Github installer and install via this app. Search for this repository or this addon name (leopheard or empirefiles)<br>
+
+![alt text](https://raw.githubusercontent.com/leopheard/plugin.video.empirefiles/master/resources/media/icon.jpeg)<br>
+
+**Screenshot**
+
+![alt text](https://raw.githubusercontent.com/leopheard/plugin.video.empirefiles/master/resources/fanart2.jpeg)
---
+++ default.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# http://www.youtube.com/user/UCG29FnXZm4F5U8xpqs1cs1Q
+#------------------------------------------------------------
+# Based on code from youtube addon
+#------------------------------------------------------------
+
+import os
+import sys
+import plugintools
+import xbmc,xbmcaddon
+from addon.common.addon import Addon
+
+addonID = 'plugin.video.empirefiles'
+addon = Addon(addonID, sys.argv)
+local = xbmcaddon.Addon(id=addonID)
+icon = local.getAddonInfo('icon')
+
+YOUTUBE_CHANNEL_ID = "UCG29FnXZm4F5U8xpqs1cs1Q"
+
+# Entry point
+def run():
+    plugintools.log("empirefiles.run")
+    
+    # Get params
+    params = plugintools.get_params()
+    
+    if params.get("action") is None:
+        main_list(params)
+    else:
+        pass
+    
+    plugintools.close_item_list()
+
+# Main menu
+def main_list(params):
+    plugintools.log("empirefiles.main_list "+repr(params))
+#note below - some YTs are /user/xxx and some /channel/xxx
+    plugintools.add_item( 
+        #action="", 
+        title="The Empire Files",
+        url="plugin://plugin.video.youtube/channel/"+YOUTUBE_CHANNEL_ID+"/",
+        thumbnail=icon,
+        folder=True )
+
+run()
